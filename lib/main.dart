02db8f2ff7b950766c01dcf61109f3d6d9730ef4
Shiny <83313977+abdul-salam111@users.noreply.github.com>
2025-05-04 01:34:06 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-
 import 'package:bookdubaisafari/app/routes/app_pages.dart';
 import 'package:bookdubaisafari/app/utils/translations/translations.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -9,12 +7,6 @@ import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_stripe/flutter_stripe.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,6 +19,9 @@ void main() async {
 
   // Set Stripe publishable key from .env
   Stripe.publishableKey = dotenv.env['STRIPE_API_KEY'] ?? '';
+  if (Stripe.publishableKey.isEmpty) {
+    throw Exception("Stripe API key is missing in .env file.");
+  }
 
   // Apply Stripe settings
   await Stripe.instance.applySettings();
@@ -38,7 +33,6 @@ void main() async {
     runApp(const MyApp());
   });
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
